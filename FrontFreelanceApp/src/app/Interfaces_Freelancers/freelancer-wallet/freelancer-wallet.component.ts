0d05eca0d730f772fad2015778d1f9538dc69b7a
Hchaichi8 <Ghassen.HCHAICHI@esprit.tn>
2026-02-11 15:58:23 +0100

    interfaces

---
+++ FrontFreelanceApp/src/app/Interfaces_Freelancers/freelancer-wallet/freelancer-wallet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer-wallet',
+  templateUrl: './freelancer-wallet.component.html',
+  styleUrl: './freelancer-wallet.component.css'
+})
+export class FreelancerWalletComponent {
+
+}
