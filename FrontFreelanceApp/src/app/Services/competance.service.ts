753c37e365917a9e5e6399b33f11532f708906eb
Hchaichi8 <Ghassen.HCHAICHI@esprit.tn>
2026-04-29 19:30:31 +0100

    Fix: Route CompetanceService through API Gateway (8085) to fix CORS

--- FrontFreelanceApp/src/app/Services/competance.service.ts
+++ FrontFreelanceApp/src/app/Services/competance.service.ts
@@ -1,4 +1,4 @@
-﻿import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Competance } from '../models/Competance';
 import { Observable } from 'rxjs';
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class CompetanceService {
 
-   constructor(private http:HttpClient) { }  
-    private url ='http://192.168.110.134:8084/Competance'
+    constructor(private http:HttpClient) { }  
+    private url ='http://192.168.110.134:8085/Competance'
 
 parseCv(file: File): Observable<Competance[]> {
   const formData: FormData = new FormData();
