ac97ce1a893c00fed5ef0e2e2f109e8a77e6d8ae
Hchaichi8 <Ghassen.HCHAICHI@esprit.tn>
2026-04-30 12:08:48 +0100

    Fix: Remove global CorsRegistry to fix duplicated headers

--- CatchOPP/MSCommunication/src/main/java/org/example/mscommunication/WebConfig.java
+++ CatchOPP/MSCommunication/src/main/java/org/example/mscommunication/WebConfig.java
@@ -19,13 +19,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + uploadPath);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://192.168.110.134")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
 
--- CatchOPP/TechnicalSupportMS/src/main/java/org/example/technicalsupport/config/JpaConfig.java
+++ CatchOPP/TechnicalSupportMS/src/main/java/org/example/technicalsupport/config/JpaConfig.java
@@ -7,13 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class JpaConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://192.168.110.134")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
 
