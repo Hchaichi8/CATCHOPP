38e0d7abeae12b2d5e3001f2962cc299ef78aa16
ayoub <ayoub.somrani@esprit.tn>
2026-05-07 01:26:53 +0100

    last

--- CATCHOPP/CatchOPP/SkillTestsMicroService/src/test/java/org/example/skilltestsmicroservice/SkillTestsMicroServiceApplicationTests.java
+++ CATCHOPP/CatchOPP/SkillTestsMicroService/src/test/java/org/example/skilltestsmicroservice/SkillTestsMicroServiceApplicationTests.java
@@ -2,9 +2,20 @@ package org.example.skilltestsmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.example.skilltestsmicroservice.Clients.UserServiceClient;
+import org.example.skilltestsmicroservice.Integration.UserInAppNotificationClient;
 
 @SpringBootTest
 class SkillTestsMicroServiceApplicationTests {
+    
+    @MockBean
+    private UserServiceClient userServiceClient;
+    
+    @MockBean
+    private UserInAppNotificationClient userInAppNotificationClient;
+    
     @Test
     void contextLoads() {}
 }
